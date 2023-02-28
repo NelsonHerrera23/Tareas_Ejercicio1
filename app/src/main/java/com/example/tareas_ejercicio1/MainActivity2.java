7d61c05aaf1b5f12d22eeb8241982cd38dd48497
NelsonHerrera23 <nelson.herrera489@gmail.com>
2023-02-27 22:47:50 -0600

    fix

--- app/src/main/java/com/example/tareas_ejercicio1/MainActivity2.java
+++ app/src/main/java/com/example/tareas_ejercicio1/MainActivity2.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
-
+//prueba
 public class MainActivity2 extends AppCompatActivity {
 
     EditText resultado;
